f817c8c44d04aeb58924cc8efba1b9fd1721c8e2
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2023-11-29 19:21:06 +0800

    build(alpine/build): :construction_worker: add build scripts for ISO generation from upstream aports
    Typical copying from my local copy, although I may check if they're outdated lol.
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>

---
+++ alpine/build/mkimg.netboot.sh
@@ -0,0 +1,32 @@
+create_image_netboot() {
+	rm -rf "${OUTDIR}"/netboot-$RELEASE "${OUTDIR}"/netboot
+	cp -aL "${DESTDIR}"/boot "${OUTDIR}"/netboot-$RELEASE
+	ln -s netboot-$RELEASE "${OUTDIR}"/netboot
+	# let webserver read initramfs
+	chmod a+r "${OUTDIR}"/netboot-$RELEASE/*
+	tar -C "${DESTDIR}" -chzf ${OUTDIR}/${output_filename} boot/
+}
+
+profile_netboot() {
+	title="Netboot"
+	desc="Kernel, initramfs and modloop for
+		netboot.
+		"
+	arch="aarch64 armhf armv7 ppc64le x86 x86_64 s390x"
+	case "$ARCH" in
+		aarch64) kernel_flavors="lts rpi rpi4 virt";;
+		armhf) kernel_flavors="rpi rpi2";;
+		armv7) kernel_flavors="lts rpi rpi2";;
+		x86_64) kernel_flavors="lts virt";;
+		*) kernel_flavors="lts";;
+	esac
+	case "$ARCH" in
+		s390x) initfs_features="base network squashfs usb dasd_mod qeth zfcp";;
+		*) initfs_features="base network squashfs usb virtio"
+	esac
+	modloop_sign=yes
+	apks=""
+	output_format="netboot"
+	image_ext="tar.gz"
+}
+
